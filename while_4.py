21ebc5dbbfa1a444d349c771f92b2dd0819dda16
Anastasiia Sidorova <nastya_8806@list.ru>
2017-03-14 23:32:03 +0300

    Home_Work_While_4

---
+++ while_4.py
@@ -0,0 +1,9 @@
+def get_answer():
+    while True:
+        user_input = input('Вопрос от пользователя - ')
+        if user_input == 'Пока':
+            break
+        else:
+            print('Пиу-пиу')
+get_answer()
+
